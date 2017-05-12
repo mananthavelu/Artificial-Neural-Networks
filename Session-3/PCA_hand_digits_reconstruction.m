0de897436be3b72511f47e48eb3a7ede9ecb920d
Mari <mari.a.muthu@gmail.com>
2017-05-12 13:39:03 +0200

    Updated

--- Session-3/PCA_hand_digits_reconstruction.m
+++ Session-3/PCA_hand_digits_reconstruction.m
@@ -1,5 +1,6 @@
 load threes -ASCII
 
+%Applying PCA
 [V, newX, D] = pca_function(threes);
 
 %Plotting the eigen values
@@ -13,27 +14,37 @@ mu = mean(threes);
 xhat = bsxfun(@minus,threes,mu); % subtract the mean
 norm(newX * V' - xhat);
 
-%figure
+%Reconstruction of original image
+figure
 fig=1;
-for i=1:4
-    
-    %Reconstruction of original image
+for i=1:4   
     Xapprox = newX(:,1:i) * V(:,1:i)';
     Xapprox = bsxfun(@plus,mu,Xapprox); % add the mean back in
-    err=sum(((threes-Xapprox).^2)/size(threes,1));
-    
+        
     %Reconstructed image
     subplot(2,2,fig)
     imagesc(reshape(Xapprox(1,:),16,16),[0,1])
-    %xlim auto
-    %ylim auto
-        
-    %plot(Xapprox(:,1),threes(:,1),'.'); hold on; plot([-4 4],[-4 4])
-    %xlabel('Approximation'); ylabel('Actual value'); grid on;
-    %xlim auto
-    %ylim auto
+    title(i);
+    xlim auto
+    ylim auto
     fig=fig+1;
 end
+
+%Reconstruction error
+Error=[];
+for i=1:50    
+    Xapprox = newX(:,1:i) * V(:,1:i)';
+    Xapprox = bsxfun(@plus,mu,Xapprox); % add the mean back in
+    err=threes-Xapprox;
+    val=norm(err,2)^2;
+    Error = [Error val];
+end
+
+figure
+plot(1:50,Error);
+title('Reconstruction Error');
+xlabel('Eigen Value')
+ylabel('Error')
 
 %figure
 %plot(Xapprox(:,1),threes(:,1),'.'); hold on; plot([-4 4],[-4 4])
